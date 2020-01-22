5e1a44de946ca769506852e0874137943b265612
TheBnn <ds.as.12@mail.ru>
2020-01-22 22:41:31 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EpamTest_task_.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
